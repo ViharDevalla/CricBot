d20bf747887388dd4089683a301c718048c24021
Vihar Devalla <vihardevalla@gmail.com>
2021-05-17 15:48:15 +0530

    Removed IPL

--- src/scoretable.py
+++ src/scoretable.py
@@ -1,4 +1,4 @@
-import requests
+""" import requests
 from html_table_parser import HTMLTableParser
 import pandas as pd
 
@@ -11,4 +11,4 @@ def scoreboard():
     p.feed(str(page.content))
     scoreboard = [p.tables[0][1],p.tables[1][1],p.tables[2][1],p.tables[3][1],p.tables[4][1],p.tables[5][1],p.tables[6][1],p.tables[7][1]]
     df = pd.DataFrame(scoreboard,columns=['Teams', 'Mat', 'Won', 'Lost', 'Tied', 'NR', 'Pts', 'NRR', '']) 
-    return(str('```'+df.to_string()+'```'))
+    return(str('```'+df.to_string()+'```')) """
